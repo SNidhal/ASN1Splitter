fccc850fb543eeb2107c2f9b918e5a1794333f0e
SNIDHAL <nidhal.saadaoui1@esprit.tn>
2020-03-31 23:16:58 +0100

    fix decoder declaration

--- src/main/java/SparkApp.scala
+++ src/main/java/SparkApp.scala
@@ -20,14 +20,15 @@ object SparkApp {
 
     val conf: Configuration = new Configuration(sparkSession.sqlContext.sparkContext.hadoopConfiguration)
 
-    //conf.setInt("mapred.max.split.size", 4000)  : To test parallelism
+    //conf.setInt("mapred.max.split.size", 4000)  //: To test parallelism
 
     val fileRDD: RDD[(LongWritable, Text)] = sparkSession.sqlContext.sparkContext
       .newAPIHadoopFile(path, classOf[AsnInputFormat], classOf[LongWritable], classOf[Text], conf)
-    val cer = new CallEventRecord
+
 
     fileRDD.map(x=>new ByteArrayInputStream(x._2.getBytes))
       .map(x=>{
+        val cer = new CallEventRecord
         cer.decode(x)
         cer
       }).collect().foreach(x=>println(x))
